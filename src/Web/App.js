f4e6870631ad7011d4394745e8fab50f4bb09f22
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-07-01 13:40:01 -0300

    lint

--- src/Web/App.js
+++ src/Web/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'
 
-import './App.css';
+import './App.css'
 
 import Canvas from './Components/Canvas'
 import Geometric from './Components/Geometric'
