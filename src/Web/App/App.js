cd5b63eea749a25e8cfa0493c15affc830909b9f
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-07-15 13:45:40 -0300

    start dropping the canvas

--- src/Web/App/App.js
+++ src/Web/App/App.js
@@ -2,19 +2,13 @@ import React from 'react'
 
 import './App.css'
 
-import Canvas from '../Components/Canvas'
-import Geometric from '../Components/Geometric'
 import Subject from '../Components/Subject'
 
 function App () {
-  const { innerWidth: width, innerHeight: height } = window
-
   return (
-    <Canvas alternative="Your browser does not support canvas" width={width} height={height}>
-      <Geometric.Circle x={35} y={35} radius={25} fill="rgb(200,0,0)" />
-      <Geometric.Circle x={55} y={55} radius={25} fill="rgba(0, 0, 200, 0.5)" />
-      <Subject x={100} y={100} radius={25} />
-    </Canvas>
+    <React.Fragment>
+      <Subject />
+    </React.Fragment>
   )
 }
 
--- src/Web/Components/Subject/index.js
+++ src/Web/Components/Subject/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 
-import Geometric from '../Geometric'
+import './subject.css'
 
-function Subject (props) {
-  const { alpha = 0, beta = 1, gama = 2 } = props
-
+function Subject () {
   return (
-    <React.Fragment>
-      <Geometric.Circle {...props} start={alpha * Math.PI} end={beta * Math.PI} fill="rgb(200,0,0)" />
-      <Geometric.Circle {...props} start={beta * Math.PI} end={gama * Math.PI} fill="rgb(0,0,200)" />
-    </React.Fragment>
+    <div className="subject"></div>
   )
 }
 
