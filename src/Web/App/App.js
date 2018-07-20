cf9570bd3353046ff859bb83e0dbd3c11248db83
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-07-19 22:09:13 -0300

    create a mother component

--- src/Web/App/App.js
+++ src/Web/App/App.js
@@ -4,12 +4,14 @@ import './App.css'
 
 import Subject from '../Components/Subject'
 import Father from '../Components/Father'
+import Mother from '../Components/Mother'
 
 function App () {
   return (
     <React.Fragment>
       <Subject />
       <Father />
+      <Mother />
     </React.Fragment>
   )
 }
---
+++ src/Web/Components/Mother/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './mother.css'
+
+function Mother () {
+  return (
+    <div className="mother"></div>
+  )
+}
+
+export default Mother
