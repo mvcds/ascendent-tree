47cc92c632f896365b9023b2286510f5812243fd
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-08-05 12:27:15 -0300

    move redux's store to an infra folder

--- src/Web/App/App.js
+++ src/Web/App/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import bem from 'bem-classname'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import Person from '../Components/Person'
 import NotificationCenter from '../Components/NotificationCenter'
 
-import rootReducer from '../reducers'
+import store from '../Infra/ReduxStore'
 
 import './App.css'
 
 const block = bem.bind(null, 'app')
-const store = createStore(rootReducer)
 
 function App () {
   return (
